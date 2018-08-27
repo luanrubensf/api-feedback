e8059b93d1e40a12143773d3821b1edeae9074eb
luanfigueredo <luanrubensf@gmail.com>
2018-08-26 22:23:23 -0300

    Creating get endpoint

---
+++ feedback/feedback.router.ts
@@ -0,0 +1,14 @@
+import {Router} from '../commons/router';
+import * as restify from 'restify';
+import {Feedback} from './feedback.model';
+
+class FeedbackRouter extends Router {
+    applyRoutes(application: restify.Server) {
+        application.get('feedbacks', (req, resp, next) => {
+            Feedback.find().then(feedbacks => {
+                resp.json(feedbacks);
+                return next();
+            })
+        });
+    }
+}
